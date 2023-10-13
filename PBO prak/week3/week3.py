daef12bd04b981e07cd3e1e85f2a11cee1accb81
Dulcoon <michael.5220411200@student.uty.ac.id>
2023-10-13 10:30:23 +0700

    gtg

--- PBO prak/week3/week3.py
+++ PBO prak/week3/week3.py
@@ -4,7 +4,9 @@ class kucing:
         self.warna = warna
 # method
     def mengeong(self):
-        print("Kucing {} dengan usia {} bulan".format(self.usia, self.warna))
+        print("Kucing {} dengan usia {} bulan".format(self.warna, self.usia ))
 
 kucing1 = kucing(8, "coklat")
+kucing2 = kucing(5, "putih")
 kucing1.mengeong()
+kucing2.mengeong()
